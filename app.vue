1d93c0e4333f86d756e8f365454a41f7593ef8bc
Rafael Santana <rsantana@colum.edu>
2024-03-27 02:16:50 -0500

    Initial commit

---
+++ app.vue
@@ -0,0 +1,47 @@
+<template>
+  <UHeader>
+    <template #logo>
+      Raph<span class="text-primary">Sant</span>
+    </template>
+    <template #right>
+      <UColorModeButton />
+
+      <UButton to="https://github.com/nuxt/ui" target="_blank" icon="i-simple-icons-github" color="gray" variant="ghost" />
+    </template>
+    <template #panel>
+      <UNavigationTree :links="links"/>
+    </template>
+  </UHeader>
+
+  <UMain>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UMain>
+
+  <UFooter>
+    <template #center>
+      Copyright © Raphsant {{ new Date().getFullYear() }}
+    </template>
+  </UFooter>
+</template>
+
+<script setup>
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-book-open',
+  to: '/'
+}, {
+  label: 'Blog',
+  icon: 'i-simple-icons-googledocs',
+  to: '/blog'
+}, {
+  label: 'LinkedIn',
+  icon: 'i-simple-icons-linkedin',
+  to: '/releases'
+}, {
+  label: 'Github',
+  icon: 'i-simple-icons-github',
+  to: '/releases'
+}]
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+    devtools: {enabled: true},
+    extends: ['@nuxt/ui-pro'],
+    modules: ['@nuxt/ui', "@nuxtjs/tailwindcss"],
+    ui: {
+        icons: ['mdi', 'simple-icons', 'heroicons'],
+        // @ts-ignore
+        variables: {
+            light: {
+                background: '255 255 255',
+                foreground: 'var(--color-gray-700)'
+            },
+            dark: {
+                background: 'var(--color-gray-900)',
+                foreground: 'var(--color-gray-200)'
+            },
+            header: {
+                height: '4rem'
+            }
+        },
+        gray: 'cool',
+
+
+    }
+})
